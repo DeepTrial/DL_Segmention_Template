3f4e8b61bef34c01fe1b525dfc6fdd99b9211525
AngularLock <resnick.xing@aol.com>
2018-05-12 22:08:56 +0800

    update centersample rule

--- perception/trainers/segmention_trainer.py
+++ perception/trainers/segmention_trainer.py
@@ -62,19 +62,31 @@ class DataGenerator():
 		self.val_gt=data[3]/255.
 		self.config=config
 
-	def _CenterSampler(self,attnlist):
+	def _CenterSampler(self,attnlist,class_weight,Nimgs):
 		"""
 		围绕目标区域采样
 		:param attnlist:  目标区域坐标
 		:return: 采样的坐标
 		"""
-
-		label=0
-		t = attnlist[label]
-		cid = random.randint(0, t[0].shape[0] - 1)
-		i_center = t[0][cid]
-		y_center = t[1][cid] + random.randint(0 - int(self.config.patch_width / 2), 0 + int(self.config.patch_width / 2))
-		x_center = t[2][cid] + random.randint(0 - int(self.config.patch_width / 2), 0 + int(self.config.patch_width / 2))
+		class_weight = class_weight / np.sum(class_weight)
+		p = random.uniform(0, 1)
+		psum = 0
+		for i in range(class_weight.shape[0]):
+			psum = psum + class_weight[i]
+			if p < psum:
+				label = i
+				break
+		if label == class_weight.shape[0] - 1:
+			i_center = random.randint(0, Nimgs - 1)
+			x_center = random.randint(0 + int(self.config.patch_width / 2), self.config.width - int(self.config.patch_width / 2))
+			# print "x_center " +str(x_center)
+			y_center = random.randint(0 + int(self.config.patch_height / 2), self.config.height - int(self.config.patch_height / 2))
+		else:
+			t = attnlist[label]
+			cid = random.randint(0, t[0].shape[0] - 1)
+			i_center = t[0][cid]
+			y_center = t[1][cid] + random.randint(0 - int(self.config.patch_width / 2), 0 + int(self.config.patch_width / 2))
+			x_center = t[2][cid] + random.randint(0 - int(self.config.patch_width / 2), 0 + int(self.config.patch_width / 2))
 
 		if y_center < self.config.patch_width / 2:
 			y_center = self.config.patch_width / 2
@@ -88,7 +100,7 @@ class DataGenerator():
 
 		return i_center, x_center, y_center
 
-	def _genDef(self,train_imgs,train_masks,attnlist):
+	def _genDef(self,train_imgs,train_masks,attnlist,class_weight):
 		"""
 		图片取块生成器模板
 		:param train_imgs: 原始图
@@ -97,11 +109,12 @@ class DataGenerator():
 		:return:  取出的训练样本
 		"""
 		while 1:
+			Nimgs=train_imgs.shape[0]
 			for t in range(int(self.config.subsample * self.config.total_train / self.config.batch_size)):
 				X = np.zeros([self.config.batch_size,self.config.patch_height, self.config.patch_width,3])
 				Y = np.zeros([self.config.batch_size, self.config.patch_height * self.config.patch_width, self.config.seg_num + 1])
 				for j in range(self.config.batch_size):
-					[i_center, x_center, y_center] = self._CenterSampler(attnlist)
+					[i_center, x_center, y_center] = self._CenterSampler(attnlist,class_weight,Nimgs)
 					patch = train_imgs[i_center, int(y_center - self.config.patch_height / 2):int(y_center + self.config.patch_height / 2),int(x_center - self.config.patch_width / 2):int(x_center + self.config.patch_width / 2),:]
 					patch_mask = train_masks[i_center, :, int(y_center - self.config.patch_height / 2):int(y_center + self.config.patch_height / 2),int(x_center - self.config.patch_width / 2):int(x_center + self.config.patch_width / 2)]
 					X[j, :, :, :] = patch
@@ -112,12 +125,14 @@ class DataGenerator():
 		"""
 		训练样本生成器
 		"""
+		class_weight=[1.0]
 		attnlist=[np.where(self.train_gt[:,0,:,:]==np.max(self.train_gt[:,0,:,:]))]
-		return self._genDef(self.train_img,self.train_gt,attnlist)
+		return self._genDef(self.train_img,self.train_gt,attnlist,class_weight)
 
 	def val_gen(self):
 		"""
 		验证样本生成器
 		"""
+		class_weight = [1.0]
 		attnlist = [np.where(self.val_gt[:, 0, :, :] == np.max(self.val_gt[:, 0, :, :]))]
-		return self._genDef(self.val_img, self.val_gt, attnlist)
+		return self._genDef(self.val_img, self.val_gt, attnlist,class_weight)
